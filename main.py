c641c4795a355ded1de8c82541c479f9d791eef4
RST2024 <rtanwani2024@gmail.com>
2024-06-28 11:04:31 -0700

    msg

--- main.py
+++ main.py
@@ -3,12 +3,25 @@ def main():
     while True:
         start = input('Enter the starting letter: ')
         end = input('Enter the starting letter: ')
+        if (start.isalpha() and len(start) ==1) and (end.isalpha and len(end) == 1):
+            start_num = ord(start)
+            end_num = ord(end)
+        else:
+            print ('Only enter single character letters')
+            continue
+        if start_num > end_num:
+            print ("The start letter needs to be before the end letter")
+            continue
+        else:
+        
 
     """
     ########################################
     Code Your Program here
     ########################################
     """
+    
+    
 
     print(*result)
 
